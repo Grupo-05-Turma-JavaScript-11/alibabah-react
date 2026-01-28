5fdd1d82f548c1cb53dc91bd66958869166eb9ff
barbieribbruna <barbieribbruna@gmail.com>
2026-01-28 13:55:50 -0300

    ajusta estrutura do layout com navbar e footer no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function App() {
+    return (
+        <BrowserRouter>
+            <Navbar />
 
-  return (
-    <>
-    </>
-  )
+            <Routes>
+                
+                <Route />
+
+            </Routes>
+
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 export default App
